a282b9b19321663e9d69b1cf25b20341a61aa541
techtutorials099 <tech.tutorials099@gmail.com>
2025-10-30 15:11:10 +0530

    v1.15

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-
-document.getElementById('app').innerHTML = `
-<div class='text-center mt-20 text-xl font-bold text-slate-700'>
-🚧 NSE Portfolio – JS code will be inserted here 🚧
-</div>`;
+<PASTE-FULL-VERSION-HERE>
